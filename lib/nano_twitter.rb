c071ed5db757301bd63f758860be19126ed95ea3
Bradford Nesbitt <bradfordnesbitt@Bradfords-MacBook-Pro.local>
2019-04-03 18:47:34 -0400

    Initial commit

--- lib/nano_twitter.rb
+++ lib/nano_twitter.rb
@@ -1,6 +1,18 @@
-require "nano_twitter/version"
+# require "nano_twitter/version"
+require 'net/http'
+require 'uri'
+require 'json'
 
 module NanoTwitter
   class Error < StandardError; end
-  # Your code goes here...
+
+  class Client
+    attr_reader :user_handle, :password, :api_path
+
+    def initialize(user_handle, password)
+      @user_handle = user_handle
+      @password = password
+      @api_path = 'https://nano-twitter-staging.herokuapp.com/api/0.5/'
+    end
+  end
 end
