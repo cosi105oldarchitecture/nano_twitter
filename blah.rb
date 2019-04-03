e6609e9c2c4b81fe54c50a21546bfa1a0e229974
Bradford Nesbitt <bradfordnesbitt@Bradfords-MacBook-Pro.local>
2019-04-03 19:21:17 -0400

    Ari fixed it

--- blah.rb
+++ blah.rb
@@ -1,25 +1,14 @@
-require 'net/http'
-require 'uri'
-require 'json'
+require 'httparty'
+require 'pry-byebug'
 require_relative './lib/nano_twitter'
 include NanoTwitter
 
 API_BASE = 'http://localhost:4567/api/0.5/login'
 client = Client.new('@ahmed1000', '@ahmed1000')
 
-uri = URI.parse(API_BASE)
-header = { 'Content-Type': 'text/json' }
 user = {
   handle: client.handle,
   password: client.password
 }
 
-# Create the HTTP objects
-http = Net::HTTP.new(uri.host, uri.port)
-request = Net::HTTP::Post.new(uri.request_uri, header)
-request.body = user.to_json
-
-# Send the request
-response = http.request(request)
-
-puts request.body
+response = HTTParty.post("#{API_BASE}/#{user[:handle]}/#{user[:password]}")
