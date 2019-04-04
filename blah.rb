f5fc53118d3a0afc96c9e2eabb574f56aeb004df
Bradford Nesbitt <bradfordnesbitt@Bradfords-MacBook-Pro.local>
2019-04-03 20:08:40 -0400

    Implements basic (CRUD) functions

--- blah.rb
+++ blah.rb
@@ -4,11 +4,13 @@ require_relative './lib/nano_twitter'
 include NanoTwitter
 
 API_BASE = 'http://localhost:4567/api/0.5/login'
+# client = Client.new('brad', '@brad', '@brad')
 client = Client.new('@ahmed1000', '@ahmed1000')
 
-user = {
-  handle: client.handle,
-  password: client.password
-}
+puts client.follow('@willow670')
+# user = {
+#   handle: client.handle,
+#   password: client.password
+# }
 
-response = HTTParty.post("#{API_BASE}/#{user[:handle]}/#{user[:password]}")
+# response = HTTParty.post("#{API_BASE}/#{user[:handle]}/#{user[:password]}")
--- lib/nano_twitter.rb
+++ lib/nano_twitter.rb
@@ -1,18 +1,53 @@
 # require "nano_twitter/version"
-require 'net/http'
-require 'uri'
-require 'json'
+require 'httparty'
 
 module NanoTwitter
   class Error < StandardError; end
 
   class Client
-    attr_reader :handle, :password, :api_path
+    attr_reader :name, :handle, :password, :api_path
 
     def initialize(handle, password)
+      @name = name
       @handle = handle
       @password = password
-      @api_path = 'https://nano-twitter-staging.herokuapp.com/api/0.5/'
+      @api_path = 'http://localhost:4567/api/0.5'
+    end
+
+    # Log into nanoTwitter
+    def login
+      HTTParty.post("#{@api_path}/login/#{@handle}/#{@password}")
+    end
+
+    # Register/create account
+    def register(name)
+      HTTParty.post("#{@api_path}/signup/#{name}/#{@handle}/#{@password}")
+    end
+
+    # Post a new Tweet
+    def tweet(tweet_body)
+      tweet_body.gsub!(/\s/, '%20')
+      HTTParty.post("#{@api_path}/tweets/new/#{@handle}/#{@password}/#{tweet_body}")
+    end
+
+    # Get list of Tweets in your timeline
+    def get_timeline
+      HTTParty.post("#{@api_path}/tweets/#{@handle}/#{@password}")
+    end
+
+    # Get list of users following you (creepy)
+    def get_followers
+      HTTParty.post("#{@api_path}/users/followers/#{@handle}/#{@password}")
+    end
+
+    # Get list of users you follow
+    def get_followees
+      HTTParty.post("#{@api_path}/users/followees/#{@handle}/#{@password}")
+    end
+
+    # Follow a new user
+    def follow(followee_handle)
+      HTTParty.post("#{@api_path}/users/follow/#{@handle}/#{@password}/#{followee_handle}")
     end
   end
 end
